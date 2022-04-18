39fc77e87580345da7901f96e5ff107a70b00144
snegha <snegha.m@tibilsolutions.com>
2022-04-18 23:20:22 +0530

    completed teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER':'postgres',
+        'PASSWORD':'snegha@2001',
+        'HOST': 'localhost',
     }
 }
 
@@ -112,6 +115,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
